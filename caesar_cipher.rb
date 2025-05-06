4ddac13eee8e57946550d3241d26b2b8252b5cd4
fragius <189782341+Fragius@users.noreply.github.com>
2025-05-06 13:21:12 +0300

    Add support for multi-word messages and special characters

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,6 @@
 input = gets.chomp.split(" ")
-message = input[0]
-shift_value = input[1].to_i
+shift_value = input.pop.to_i
+message = input.join(" ")
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 def is_downcase? (letter)
@@ -8,11 +8,15 @@ def is_downcase? (letter)
 end
 
 def get_letter_index(letter, string)
-  string.index(letter.downcase)
+  letter = letter.downcase
+  unless string.include?(letter)
+    return "spcc" #spcc stands for special-character
+  end
+  string.index(letter)
 end
 
 def shift_letter (letter_index, shift_value, string, is_downcase)
-    new_index = (string.length - (shift_value + letter_index)) * -1
+    new_index = (string.length - (shift_value + letter_index)) * - 1
     unless is_downcase 
       return string[new_index].upcase
     end
@@ -23,7 +27,11 @@ def encrypt(message, shift_value, alphabet)
   encrypted_message = ""
   message.each_char do |char| 
     letter_index = get_letter_index char, alphabet
-    new_letter = shift_letter letter_index, shift_value, alphabet, is_downcase?(char)
+    if letter_index == "spcc"
+      new_letter = char
+    else 
+      new_letter = shift_letter letter_index, shift_value, alphabet, is_downcase?(char)
+    end
     encrypted_message += new_letter
   end
   encrypted_message
